4446aaaabd2129c363bf52c09d552e702d6228a8
student3B032081 <3b032081@gm.student.ncut.edu.tw>
2024-12-24 16:47:13 +0800

    練習2-2

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title') | My Blog</title>
+</head>
+<body>
+@include('layouts.partials.navigation')
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
+
